5972df86ddde3443fe4f8b5233e9a5261af9464b
Yousef Khell <yousef@khell.com>
2022-06-09 19:51:34 +0300

    Added i3 installation and config

---
+++ .config/autostart.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+#####################################3
+# Keymaps 
+
+# Make CAPS lock an extra escape key
+setxkbmap -option caps:escape
+
+# Compositor
+killall compton
+compton
+
+# Restore Wallpaper
+nitrogen --Restore
+
+# Polybar
+killall -q polybar
+while pgrep -x polybar >/dev/null; do sleep; done
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload mainbar-i3 &
+    done
+else
+    polybar --reload mainbar-i3 &
+fi
---
+++ INSTALL.sh
@@ -0,0 +1,10 @@
+# _____  __      ______________________
+# __  / / /_________  __/_  __ \_  ___/
+# _  / / /__  ___/_  /_ _  / / /____ \ 
+# / /_/ / _(__  )_  __/ / /_/ /____/ / 
+# \____/  /____/ /_/    \____/ /____/  
+#
+# UsfOS Debian Edition
+
+# Install apt Packages
+apt install git vim neovim dmenu rofi arandr nitrogen i3 compton ranger
